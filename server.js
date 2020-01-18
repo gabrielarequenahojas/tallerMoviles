1121ccb8ed82293e3057c4b3a9cb428216fd3f74
gabrielarequenahojas <dsgonzag@espol.edu.ec>
2020-01-17 22:11:20 -0500

    server

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
-
-const server = create();
-const router = router('taller.json');
-const middlewares = defaults();
+const jsonServer = require('json-server');
+const server = jsonServer.create();
+const router = jsonServer.router('db.json');
+const middlewares = jsonServer.defaults();
 const port = process.env.PORT || 3000;
 
 server.use(middlewares);
